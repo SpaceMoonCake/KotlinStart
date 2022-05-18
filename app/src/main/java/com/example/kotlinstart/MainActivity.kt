9f4b032970d8632e786f6834e1b22b9f3d54177b
moonCake <klamina259@gmail.com>
2022-05-18 22:36:11 +0300

    done

--- app/src/main/java/com/example/kotlinstart/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstart/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.kotlinstart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var dog = SuperDog.superDog.copy("Почти супер")
+    val cycles = Cycles()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initView()
     }
+
+    private fun initView() {
+        initButton()
+        initTextView();
+    }
+
+    private fun initButton() {
+        val button: Button = findViewById(R.id.button_click_on_me)
+        button.setOnClickListener{
+            Toast.makeText(this, "You click on me!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initTextView() {
+        var speciesDog: TextView = findViewById(R.id.text_species)
+        var nameDog: TextView = findViewById(R.id.text_name)
+        speciesDog.text = dog.species
+        nameDog.text = dog.name
+    }
+
+
 }
---
+++ app/src/main/java/com/example/kotlinstart/SuperDog.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinstart
+
+object SuperDog {
+    val superDog = Dog("Супер", "Супер")
+}
